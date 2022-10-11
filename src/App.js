02b87b311dc2fe718b7924bd8cf3335ea160bd53
HamimHossain <hamim.mgp@gmail.com>
2022-10-11 11:18:55 +0600

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './routes';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>This is assignment nine.....</h1>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import About from "./components/About";
+import Blog from "./components/Blog";
+import Root from "./components/Root";
+import Home from './components/Home';
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root></Root>,
+        children:[
+            {path: '/', element: <Home></Home>},
+            {path: 'blog', element: <Blog></Blog>},
+            {path: 'about', element: <About></About>},
+        ]
+    },
+])
+
+export default router
