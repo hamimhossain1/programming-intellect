fa560103e7208885a8ca480145a6a8e4f41e10b8
HamimHossain <hamim.mgp@gmail.com>
2022-10-12 18:32:46 +0600

    quiz option true false almost done

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,41 +1,67 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {  faEye } from '@fortawesome/free-solid-svg-icons'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
 import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.min.css'; 
+import 'react-toastify/dist/ReactToastify.min.css';
 
 const Quiz = ({ quiz }) => {
     const { id, question, correctAnswer, options } = quiz;
     console.log(quiz)
 
-    const correctAnswerShow = ()=>{
-        console.log(correctAnswer)
-        const correctAns = correctAnswer;
-        toast(correctAns);
+    const correctAnswerShow = () => {
+        toast(correctAnswer);
     }
-   
+
     return (
         <div>
             <div className="card w-11/12 md:w-2/4 bg-base-300 shadow-xl mx-auto mb-7 mt-5">
                 <div className="card-body relative ">
                     <h2 className="card-title text-green-900">Question:  {question}.</h2>
-                    
+
                     <div className='grid gap-5 mt-8'>
                         {
-                            options.map(option => <button className='text-left text-amber-900'
-                            key={option.index}
-                            > <input type="radio" name="radio-1" className="radio" />  {option}</button>)
+                            options.map(option =>
+                                <div >
+                                    <p id='opt' onClick={e => {
+                                        if(e.target.innerText.replace(/\s/g, '') === correctAnswer.replace(/\s/g, ''))
+                                        {
+                                         toast("Correct");   
+                                        }
+                                        else{
+                                            toast("Wrong");
+                                        }
+                                    }
+
+                                    }>  {option}</p>
+                                </div>
+                            )
                         }
+                        
                     </div>
-                    <button onClick={()=> correctAnswerShow(quiz)}>
-                    <FontAwesomeIcon  className='absolute top-1 text-purple-800 text-2xl right-3' icon={faEye}></FontAwesomeIcon>
+
+                    <button onClick={() => correctAnswerShow(quiz)}>
+                        <FontAwesomeIcon className='absolute top-1 text-purple-800 text-2xl right-3' icon={faEye}></FontAwesomeIcon>
                     </button>
                     
+
                 </div>
             </div>
-                    <ToastContainer position='top-center'  autoClose={1000}/>
+            <ToastContainer position='top-center' autoClose={1000} />
         </div>
     );
 };
 
 export default Quiz;
+
+
+
+// {
+//     options.map(option =>
+    
+//         <button
+//     className='text-left text-amber-900'
+//         key={option.index}
+
+        
+//     > <input type="radio" name="radio-1" className="radio" />  {option}</button>)
+// }
