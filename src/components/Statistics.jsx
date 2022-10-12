a96d2375c4bb3228a258e3ed1f91628e1023caa7
HamimHossain <hamim.mgp@gmail.com>
2022-10-12 09:38:05 +0600

    chart done

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { TopicsContext } from './Root';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
+    const topics = useContext(TopicsContext);
+    console.log(topics)
     return (
         <div>
-            <h1>This is statistics</h1>
+            <h1 className='text-3xl font-bold'>This is statistics</h1>
+            <LineChart className='mx-auto my-16' width={800} height={500} data={topics}>
+            <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+            <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+            </LineChart>
         </div>
     );
 };
