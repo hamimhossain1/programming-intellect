767a6c0dc8529550951e90f022e79b8355685c0c
HamimHossain <hamim.mgp@gmail.com>
2022-10-12 14:32:53 +0600

    rechart responsive done

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { TopicsContext } from './Root';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
     const topics = useContext(TopicsContext);
@@ -8,13 +8,25 @@ const Statistics = () => {
     return (
         <div>
             <h1 className='text-3xl font-bold'>This is statistics</h1>
-            <LineChart className='mx-auto my-16' width={800} height={500} data={topics}>
-            <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-            <Line type="monotone" dataKey="total" stroke="#82ca9d" />
-            </LineChart>
-        </div>
+
+            <ResponsiveContainer className='mx-auto' width="60%" aspect={3}>
+            <LineChart className=' my-16'
+                width={800}
+                height={500}
+                data={topics}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                 }} >
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+                </LineChart>
+            </ResponsiveContainer>
+        </div >
     );
 };
 
